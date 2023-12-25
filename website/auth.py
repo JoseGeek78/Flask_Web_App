25cfdff4c9e2d92a48c2689fdef483cc309cafa3
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-25 17:20:59 +0100

    Updated

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
 
+
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return '<p>Login</p>'
+
+@auth.route('/logout')
+def login():
+    return '<p>Logout</p>'
+
+@auth.route('/sign-up')
+def login():
+    return '<p>Sign Up</p>'
